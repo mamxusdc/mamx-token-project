a7e3eb1fff568dcc5b37f48e0cbb6750dec47b3d
mamxusdc <mamxusdc@gmail.com>
2025-05-08 12:07:15 +0700

    Update MAMXToken.sol

--- contracts/MAMXToken.sol
+++ contracts/MAMXToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.28;
 
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.9/contracts/token/ERC20/ERC20.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.9/contracts/access/Ownable.sol";
