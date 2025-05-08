e245a0c691dd4bb1292b91ccf6f8e7a2a3b99c0a
mamxusdc <mamxusdc@gmail.com>
2025-05-08 10:57:52 +0700

    Update MAMXToken.sol
    简化为无增发/销毁的标准 MAMXToken 合约

--- contracts/MAMXToken.sol
+++ contracts/MAMXToken.sol
@@ -1,79 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface IBEP20 {
-    function totalSupply() external view returns (uint256);
-    function decimals() external pure returns (uint8);
-    function symbol() external pure returns (string memory);
-    function name() external pure returns (string memory);
-    function getOwner() external view returns (address);
-    
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    
-    function allowance(address _owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.9/contracts/token/ERC20/ERC20.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.9/contracts/access/Ownable.sol";
 
-contract MAMXToken is IBEP20 {
-    string public constant override name = "MAMX Token";
-    string public constant override symbol = "MAMX";
-    uint8 public constant override decimals = 18;
-
-    uint256 private _totalSupply = 2_100_000_000 * 10**uint256(decimals);
-    address private _owner;
-
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    constructor() {
-        _owner = msg.sender;
-        _balances[_owner] = _totalSupply;
-        emit Transfer(address(0), _owner, _totalSupply);
-    }
-
-    function getOwner() external view override returns (address) {
-        return _owner;
-    }
-
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view override returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address recipient, uint256 amount) external override returns (bool) {
-        require(_balances[msg.sender] >= amount, "Insufficient balance");
-        _balances[msg.sender] -= amount;
-        _balances[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) external override returns (bool) {
-        _allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
-        require(_balances[sender] >= amount, "Insufficient balance");
-        require(_allowances[sender][msg.sender] >= amount, "Allowance exceeded");
-
-        _balances[sender] -= amount;
-        _balances[recipient] += amount;
-        _allowances[sender][msg.sender] -= amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
+contract MAMXToken is ERC20, Ownable {
+    constructor() ERC20("MAMX Token", "MAMX") {
+        _mint(msg.sender, 2_100_000_000 * 10 ** decimals());
     }
 }
